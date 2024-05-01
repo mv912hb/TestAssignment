5220bf00d78660317bd9d1bc4345ee31c4cab277
Boris Vedernikov <boris.v@mes-ltd.com>
2024-05-01 10:17:56 +0300

    namings adjustments

--- TestAssignment/Tests/WikipediaApiTests.cs
+++ TestAssignment/Tests/WikipediaApiTests.cs
@@ -15,12 +15,12 @@ public class WikipediaApiTests : BaseClass
             .Navigate()
             .GetDataFromPage();
 
-        var notFoundInApi = dataFromApi
+        var notFoundInUi = dataFromApi
             .Where(item =>
                 !dataFromUi.Keys.Any(key => string.Equals(key, item.Key, StringComparison.OrdinalIgnoreCase)))
             .ToList();
 
-        Assert.That(notFoundInApi, Is.Empty,
-            $"Word occurrences from API call not found in UI parsing: {string.Join(Environment.NewLine, notFoundInApi)}");
+        Assert.That(notFoundInUi, Is.Empty,
+            $"Word occurrences from API call not found in UI parsing: {string.Join(Environment.NewLine, notFoundInUi)}");
     }
 }
